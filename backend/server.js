e966bf8c93afc5cab95cbf6864521fffb70d5685
f5rcbh <f6807@hotmail.com>
2021-12-24 15:40:20 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,24 +26,49 @@ app.get("/f5rcbh", (req, res) => {
   });
 });
 
-app.post("/post", (req, res) => {
+app.post("/f5rcbh", (req, res) => {
   Todo.create(req.body, (err, data) => {
     if (err) {
       console.log("psot error", err);
     } else {
       res.json(data);
-      console.log("post done", data);
+      console.log("post done");
     }
   });
 });
 
-app.delete("/D", (req, res) => {
-  Todo.deleteMany({}, (err, data) => {
+app.delete("/f5rcbh/:id", (req, res) => {
+  Todo.deleteOne({ _id: req.params.id }, (err, deleteData) => {
     if (err) {
       console.log("delete error ", err);
     } else {
-      res.json("delete one elm", data);
-      console.log("delete one elm", data);
+      if (deleteData.deletedCount === 1) {
+        res.json("delete one elm");
+        console.log("delete one elm");
+      } else {
+        res.status(404).json("delete NOT FOUND");
+      }
     }
   });
+});
+
+app.put("/f5rcbh/:id", (req, res) => {
+  Todo.updateOne(
+    { _id: req.params.id },
+    { title: req.body.newTitle },
+    (err, updateData) => {
+      if (err) {
+        console.log("update Error", err);
+        res.status(404).json(err);
+      } else {
+        if (updateData.modifiedCount === 1) {
+          res.json("update one elm");
+          console.log("update one elm");
+        } else {
+          console.log(err);
+          res.status(404).json("update NOT FOUND");
+        }
+      }
+    }
+  );
 });
