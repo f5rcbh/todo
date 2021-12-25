2b06f825677a3aed2ffb6d2b0bfa4beeacbb61dd
f5rcbh <f6807@hotmail.com>
2021-12-25 12:50:13 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,20 @@ app.get("/f5rcbh", (req, res) => {
   });
 });
 
+// for filter data
+
+app.get("/f5rcbh/complete", (req, res) => {
+  console.log(req.query);
+  Todo.find({ isCompleted: req.query.isCompleted }, (err, data) => {
+    if (err) {
+      console.log("get error", err);
+    } else {
+      res.json(data);
+    }
+    console.log("get done");
+  });
+});
+
 app.post("/f5rcbh", (req, res) => {
   Todo.create(req.body, (err, data) => {
     if (err) {
@@ -52,6 +66,21 @@ app.delete("/f5rcbh/:id", (req, res) => {
   });
 });
 
+app.delete("/f5rcbh", (req, res) => {
+  Todo.deleteMany({ isCompleted: true }, (err, deleteData) => {
+    if (err) {
+      console.log("delete error ", err);
+    } else {
+      if (deleteData.deletedCount === 0) {
+        res.status(404).json("delete NOT FOUND");
+      } else {
+        res.json("delete all elm");
+        console.log("delete all elm", deleteData);
+      }
+    }
+  });
+});
+
 app.put("/f5rcbh/:id", (req, res) => {
   Todo.updateOne(
     { _id: req.params.id },
@@ -67,6 +96,28 @@ app.put("/f5rcbh/:id", (req, res) => {
         } else {
           console.log(err);
           res.status(404).json("update NOT FOUND");
+        }
+      }
+    }
+  );
+});
+
+app.put("/f5rcbh/:id/:isCompleted", (req, res) => {
+  console.log(req.params);
+  Todo.updateOne(
+    { _id: req.params.id },
+    { isCompleted: req.params.isCompleted },
+    (err, updateData) => {
+      if (err) {
+        console.log("update Error", err);
+        res.status(404).json(err);
+      } else {
+        if (updateData.modifiedCount === 0) {
+          console.log(err);
+          res.status(404).json("update NOT FOUND");
+        } else {
+          res.json("update all elm");
+          console.log("update all elm", updateData);
         }
       }
     }
